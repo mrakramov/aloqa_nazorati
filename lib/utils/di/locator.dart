adad7e0da70074c0de3dd048c342d59d84194cf3
Axmadjon Isaqov <67532499+axmadjon2000@users.noreply.github.com>
2022-08-04 20:58:38 +0500

    google maps qoshildi

--- lib/utils/di/locator.dart
+++ lib/utils/di/locator.dart
@@ -5,7 +5,7 @@ import 'package:get_it/get_it.dart';
 import '../db/hive_db.dart';
 
 GetIt serviceLocator = GetIt.instance;
-//setup
+//service locator
 void setup() async {
   serviceLocator.registerLazySingleton<FileRepo>(() => FileRepo());
   serviceLocator.registerSingleton<HiveDB>(HiveDB());
